ed1e6f3ca14ec734edeb55ee79dbfdacfd72252e
Nguyễn Quốc Trọn <quoctron.200901@gmail.com>
2024-12-27 01:00:36 +0700

    feat: add notification feature to header with modal and filter options

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
-import { Menu } from "antd";
-import { UserOutlined, ShoppingCartOutlined } from "@ant-design/icons";
-import "./Header.css";
-import logoImage from "../../assets/images/logo.png";
+import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
+import { Badge, Menu, Modal, List, Tag, Select } from "antd";
+import { useState } from "react";
+import { FaBell } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import logoImage from "../../assets/images/logo.png";
 import NotificationBell from "../../pages/admin/notification-management/NotificationBell";
-
+import "./Header.css";
+const notifications = [
+  {
+    id: 1,
+    message: "Đơn hàng của bạn đã được xác nhận.",
+    status: "Đã xem",
+    time: "19/12/2024 08:00",
+    isRead: true,
+  },
+  {
+    id: 2,
+    message: "Đơn hàng của bạn đang được giao.",
+    status: "Chưa xem",
+    time: "19/12/2024 10:00",
+    isRead: false,
+  },
+  {
+    id: 3,
+    message: "Đơn hàng đã được giao thành công.",
+    status: "Chưa xem",
+    time: "18/12/2024 15:30",
+    isRead: false,
+  },
+];
 function Header() {
   const navigate = useNavigate();
   const [current, setCurrent] = useState("home"); // Set giá trị mặc định cho current
   const roleAdmin = localStorage.getItem("isAdmin");
   const accessToken = localStorage.getItem("accessToken");
+  const [open, setOpen] = useState(false);
   console.log(!!accessToken);
   // Hàm xử lý sự kiện cho từng mục và chuyển hướng bằng navigate
   const handleItemClick = (e) => {
@@ -82,9 +106,7 @@ function Header() {
     {
       label: "Portfolio",
       key: "portfolio",
-      children: [
-        { label: "Photography", key: "photo" },
-      ],
+      children: [{ label: "Photography", key: "photo" }],
     },
     {
       label: "Cửa hàng",
@@ -135,13 +157,11 @@ function Header() {
   ];
 
   return (
-    
     <div className="header-container">
       <div onClick={() => navigate("/")} className="logo">
         <img src={logoImage} alt="A-Z Studio" className="logo-image" />
       </div>
       <div className="navbar">
-        
         <Menu
           onClick={handleItemClick}
           selectedKeys={[current]} // Hiển thị mục đang chọn
@@ -149,6 +169,58 @@ function Header() {
           items={[...items, ...rightItems]} // Kết hợp menu items chính và items bên phải
           className="main-menu"
         />
+        <Badge count={3}>
+          <FaBell
+            onClick={() => setOpen(true)}
+            size={20}
+            className="cursor-pointer"
+          />
+        </Badge>
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          onCancel={() => setOpen(false)}
+          onOk={() => setOpen(false)}
+          title="Thông báo"
+        >
+          <div className="p-4 max-w-md mx-auto">
+            <Select
+              defaultValue={"all"}
+              className="w-full"
+              placeholder="Filter"
+              options={[
+                {
+                  value: "all",
+                  label: "Tất cả",
+                },
+                {
+                  value: "isRead",
+                  label: "Đã xem",
+                },
+                {
+                  value: "isNotRead",
+                  label: "Chưa xem",
+                },
+              ]}
+            />
+            <List
+              dataSource={notifications}
+              renderItem={(item) => (
+                <List.Item className="border-b last:border-none w-full">
+                  <div className="flex flex-col w-full">
+                    <div className="flex justify-between items-center mb-2 justify-between w-full ">
+                      <span>{item.message}</span>
+                      <Tag color={item.isRead ? "green" : "red"}>
+                        {item.isRead ? "Đã xem" : "Chưa xem"}
+                      </Tag>
+                    </div>
+                    <div className="text-gray-500 text-sm">{item.time}</div>
+                  </div>
+                </List.Item>
+              )}
+            />
+          </div>
+        </Modal>
       </div>
     </div>
   );
